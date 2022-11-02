6312cb4bc31a6b2bfe878e0efb9755dbe17a88f7
badihi <badihyuval2@gmail.com>
2022-11-02 17:00:25 +0200

    badihi push try

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestThreadCheckArray {
-//	badihi parts
+//	badihi part
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
 			Thread thread1, thread2;
